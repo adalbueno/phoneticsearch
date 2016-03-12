ccf2df314f1e3ce05e06aa6d2a31ebb2c3e79e0f
Adal Bueno <adal@adalbueno.com>
2016-03-12 12:44:20 -0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var stylish = require('gulp-jscs-stylish');
+var runSequence = require('run-sequence');
+var connect = require('gulp-connect');
+
+require('gulp-watch');
+
+var bases = {
+  src: 'src/',
+  dest: 'dist/'
+};
+
+var paths = {
+  html: ['**/*.html'],
+  js: ['assets/**/*.js'],
+  css: ['assets/**/*.scss']
+};
+
+gulp.task('clean', function(callback) {
+  return gulp.src(bases.dest, {read:false}, callback)
+    .pipe(clean({force: true}));
+});
+
+gulp.task('html', function() {
+  return gulp.src(paths.html, {cwd: bases.src})
+    .pipe(gulp.dest(bases.dest))
+    .pipe(connect.reload())
+});
+
+gulp.task('js', function() {
+  return gulp.src(paths.js, {cwd: bases.src})
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(bases.dest + 'assets/js/'))
+    .pipe(connect.reload())
+});
+
+gulp.task('css', function() {
+  return gulp.src(paths.css, {cwd: bases.src})
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest(bases.dest + 'assets/css/'))
+    .pipe(connect.reload())
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: bases.dest,
+    port: 8000,
+    livereload: true
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.html, {cwd: bases.src}, ['html']);
+  gulp.watch(paths.js, {cwd: bases.src}, ['js']);
+  gulp.watch(paths.css, {cwd: bases.src}, ['css']);
+});
+
+gulp.task('default', function(callback) {
+  runSequence('clean', ['html', 'js', 'css'], callback);
+});
+
+gulp.task('server', function(callback) {
+  runSequence('default', 'connect', 'watch', callback);
+});
---
+++ src/assets/js/node.js
@@ -0,0 +1,5 @@
+var phonetic = require('./phonetic.js');
+
+var input = ['1ton#', 'brief', 'soon'];
+var dictionary = ['angitem', 'brave', 'Braev', 'Don', 'Engitem', 'go', 'goal', 'son', 'sunny', 'Tom', 'Tooonnnnyyyy'];
+
